0d6056f3d09b6e54f1e1377af9028898fd064f0b
Lawlesh2022 <106768533+Lawlesh2022@users.noreply.github.com>
2022-07-30 14:54:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+const date = new Date();
+const renderCalendar = function() {
+    const monthDays = document.querySelector(".days");
+    const lastDay = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate();
+    const prevLastDay = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate();
+    const firstDayIndex = date.getDay();
+    const lastDayIndex = new Date(date.getFullYear(), date.getMonth()+1, 0).getDay();
+    const nextDays = 7- lastDayIndex;
+    const months = [
+        "January", 
+        "February", 
+        "March", 
+        "April", 
+        "May", 
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+    document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+    document.querySelector(".date p").innerHTML = date.toDateString();
+    let days = "";    
+    for(let x = firstDayIndex; x>0; x--){
+        days = days+ "<div class = 'prev-date'>" + (prevLastDay-x+1) + "</div>";
+    }
+    for(let i=1; i<=lastDay; i++){
+        if(i===new Date().getDate() && date.getMonth()===new Date().getMonth()){
+        days = days + "<div class = 'today'>" + i + "</div>";
+        }else{
+            days = days + "<div>" + i + "</div>";
+        }
+        monthDays.innerHTML = days;
+    }
+    for(let j=1; j<=nextDays; j++){
+        days = days +"<div class = 'next-date'>" +j + "</div>";
+        monthDays.innerHTML = days;
+    }
+} 
+document.querySelector(".prev").addEventListener("click", function(){
+    date.setMonth(date.getMonth()-1);
+    renderCalendar();
+});
+document.querySelector(".next").addEventListener("click", function(){
+    date.setMonth(date.getMonth()+1);
+    renderCalendar();
+});
+renderCalendar();
